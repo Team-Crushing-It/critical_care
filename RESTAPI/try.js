a9b4e1989e062e820d093a1133ca0480865a1db8
mihirKachroo <mkachroo29@gmail.com>
2020-08-02 22:14:49 -0400

    Bloc Format

--- RESTAPI/try.js
+++
@@ -1,6 +0,0 @@
-const userAction = async () => {
-    const response = await fetch('https://uploaded-report-api.herokuapp.com/');
-    const myJson = await response.json(); //extract JSON from the http response
-    // do something with myJson
-    console.log(myJson);
-  }
