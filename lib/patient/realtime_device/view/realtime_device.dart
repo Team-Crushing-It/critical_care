6b5dfefb733094bc9826ffa00013c8a36a779f4d
mihirKachroo <mkachroo29@gmail.com>
2020-08-13 11:51:19 -0400

    Styling Added

--- lib/patient/realtime_device/view/realtime_device.dart
+++ lib/patient/realtime_device/view/realtime_device.dart
@@ -67,8 +67,7 @@ class _RealtimeDeviceSuccessView extends StatelessWidget {
           children: devices.map(
             (device) {
               return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 7),
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
                 child: Container(
                   width: double.infinity,
                   // height: 1,
@@ -97,69 +96,86 @@ class _DeviceView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Stack(
+        Row(
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 8.0, top: 0.5),
+              padding: const EdgeInsets.only(left: 33.0, top: 10),
               child: SizedBox(
                 width: 100,
-                height: 100,
-                child: Image.network(
-                  device.graph,
-                  fit: BoxFit.cover,
-                ),
+                height: 75,
+                child: Stack(children: [
+                  Image.network(
+                    device.graph,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                      alignment: Alignment(0.9, -0.8),
+                      child: Text(
+                        device.range1,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 7.0),
+                      )),
+                  Container(
+                      alignment: Alignment(0.9, 0.78),
+                      child: Text(
+                        device.range2,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 7.0),
+                      )),
+                ]),
               ),
             ),
-            Container(
-                alignment: Alignment.topRight,
-                child: Text(
-                  'Text',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 6.0),
-                )),
-          ],
-        ),
-        SizedBox(width: 11),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Column(
+            SizedBox(width: 11),
+            Row(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(device.type,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(device.type,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline2
+                            .copyWith(color: device.type.toColor)),
+                    Text(device.unit,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline1
+                            .copyWith(color: device.type.toColor)),
+                    Text(device.range1,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline1
+                            .copyWith(color: device.type.toColor)),
+                    Text(device.range2,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline1
+                            .copyWith(color: device.type.toColor)),
+                  ],
+                ),
+                SizedBox(width: 10),
+                Text(device.value,
                     style: Theme.of(context)
                         .textTheme
-                        .headline2
-                        .copyWith(color: device.type.toColor)),
-                Text(device.unit,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline1
-                        .copyWith(color: device.type.toColor)),
-                Text(device.range1,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline1
-                        .copyWith(color: device.type.toColor)),
-                Text(device.range2,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline1
+                        .subtitle1
                         .copyWith(color: device.type.toColor)),
               ],
             ),
-            SizedBox(width: 10),
-            Text(device.value,
-                style: Theme.of(context)
-                    .textTheme
-                    .subtitle1
-                    .copyWith(color: device.type.toColor)),
           ],
-        )
+        ),
+        Divider(
+          color: Color(0xFFC4C4C4),
+          height: 25,
+          thickness: 1,
+          endIndent: 0,
+        ),
       ],
     );
   }
