9a78385429e6b050133b538bc55b28083a095b32
Gene <45580241+Gene-Dana@users.noreply.github.com>
2020-08-14 16:45:56 -0400

    new computer

--- lib/rescale.py
+++ lib/rescale.py
@@ -1,31 +1,29 @@
-from math import sin
+inputvalue = [
+     50,
+    50,
+    50,
+    65,
+    50,
+    100,
+    15,
+    65,
+    50,
+    50,
+    50,
+    50,
+    50,
+    50,
+    50,
+]
 
-def sineFunctionThing(x):
-  beforesined = 1 / 3.03 * x;
-  sined = sin(beforesined);
-  ybefore = 40*sined;
-  y = int(ybefore);
-  print(y+50);
+newvalues = []
+counter = 0
+print("start")
+while counter < len(inputvalue)-1:
+    tempint=(inputvalue[counter+1]-inputvalue[counter])/2+inputvalue[counter]
+    newvalues.append(inputvalue[counter])
+    newvalues.append(tempint)
+    newvalues.append(inputvalue[counter+1])
+    counter+=1
 
-sineFunctionThing(0);
-sineFunctionThing(1);
-sineFunctionThing(2);
-sineFunctionThing(3);
-sineFunctionThing(4);
-sineFunctionThing(5);
-sineFunctionThing(6);
-sineFunctionThing(7);
-sineFunctionThing(8);
-sineFunctionThing(9);
-sineFunctionThing(10);
-sineFunctionThing(11);
-sineFunctionThing(12);
-sineFunctionThing(13);
-sineFunctionThing(14);
-sineFunctionThing(15);
-sineFunctionThing(15);
-sineFunctionThing(16);
-sineFunctionThing(17);
-sineFunctionThing(18);
-sineFunctionThing(19);
-
+print(newvalues)
